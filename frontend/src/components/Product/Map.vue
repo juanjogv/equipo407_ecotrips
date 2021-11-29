47e6e4917a427fa4efe892d9d5ef7f695509073d
juanjogv <juangarciavxx@gmail.com>
2021-11-29 01:03:15 -0500

    recibiendo todos los datos necsarios para mostrar el servicio en el mapa junto con un marcador

--- frontend/src/components/Product/Map.vue
+++ frontend/src/components/Product/Map.vue
@@ -7,24 +7,25 @@ import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { onMounted } from "vue";
 export default {
-  setup() {
+  name: "Map",
+  props: { coordsLng: String, coordsLat: String },
+  data() {
+    return {};
+  },
+  setup(props) {
     onMounted(() => {
       mapboxgl.accessToken = "pk.eyJ1IjoianVhbmpvZ3YiLCJhIjoiY2tzdGE5bjFmMTN6dzJvcG84a2locHA0MSJ9.JuMq5YMkJnQZQmpU5U5RQA";
       const map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11", // style URL
-        center: [-75.6015099, 6.2456602], // starting position [lng, lat]
-        zoom: 15, // starting zoom
+        center: [parseFloat(props.coordsLng), parseFloat(props.coordsLat)], // starting position [lng, lat]
+        zoom: 12, // starting zoom
       });
-      map.on("load", () => {
-        console.log("me cargue");
-        // TODO: Here we want to load a layer
-        // TODO: Here we want to load/setup the popup
-      });
-      new mapboxgl.Marker().setLngLat([-75.6015099, 6.2456602]).addTo(map);
+      new mapboxgl.Marker().setLngLat([parseFloat(props.coordsLng), parseFloat(props.coordsLat)]).addTo(map);
     });
     return {};
   },
+  mounted() {},
 };
 </script>
 
